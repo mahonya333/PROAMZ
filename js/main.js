68b01dd93a66ffbd687db357726d5ad498f926e9
mahonya333 <mahonya333@gmail.com>
2019-11-21 11:24:46 +0200

    added data-slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
-    $('.slider__items').slick({
+/*     $('.slider__items').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/arrow-left.svg" alt=""> prev</button>',
-        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/arrow-right.svg" alt=""> next</button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next">next <img src="images/arrow-right.svg" alt=""></button>',
     });
     $(".slider__items").on('afterChange', function(event, slick, currentSlide){
         $(".slider-number span").text(currentSlide + 1);
@@ -11,4 +11,9 @@ $(function () {
         prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/arrow-right.svg" alt=""></button>',
     });
+    $ ('.about__schedule').daterangepicker({
+        opens: 'center',
+        drops: 'up', 
+    }); */
+    $('input[type="file"], select').styler();
 });
